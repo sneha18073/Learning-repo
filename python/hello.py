46d7c42ada161c1d3a873d8da278c0b53a2d9b2d
Sneha N <snehan1807@gmail.com>
2024-02-02 23:41:17 +0530

    python basics

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,5 @@
 print("hello world!")
+
+# Practice set - 01
+# write a program to print twinkle twinkle little star
+print(" Twinkle Twinkle Little star\n How I wonder what you are\n Up above the world so high\n Like a diamond in the sky")
