2f98ab5f216c21e04c8bc201782796da1aa7dcd6
Sneha N <snehan1807@gmail.com>
2024-02-04 23:23:40 +0530

    python practice

--- python/hello.py
+++ python/hello.py
@@ -1,5 +1,25 @@
+import numpy
+import os
 print("hello world!")
 
 # Practice set - 01
 # write a program to print twinkle twinkle little star
 print(" Twinkle Twinkle Little star\n How I wonder what you are\n Up above the world so high\n Like a diamond in the sky")
+
+#print tables of 5 in REPL (Read-Evaluate-Print-Loop)
+
+#Install an external module and run a command
+'''a1D = np.array([1, 2, 3, 4])
+
+a2D = np.array([[1, 2], [3, 4]])
+
+a3D = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+print(a1D, a2D, a3D)
+'''
+#print some functionalities of in-built modules and it's functions
+print(os.name)
+print(os.ctermid)
+print(os.path)
+
+#Demonstrate usees of comments ==> Done
