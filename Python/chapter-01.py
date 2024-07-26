ab32482a29ff513555ee73d5d0d446515f26bacb
Sneha N <snehan1807@gmail.com>
2024-07-26 22:47:19 +0530

    chapter-01 of python Modules, comments and pips

---
+++ Python/chapter-01.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+
+print("Twinkel twinkle little star")
+
+import numpy as np
+array = np.array([1, 2, 3])
+print(np.mean(array))  # Output: 2.0
+
+
+'''import os
+
+# Specify the directory you want to list
+directory_path = 'path/to/your/directory'
+
+# Get the list of files and directories in the specified directory
+contents = os.listdir(directory_path)
+
+# Print each item in the directory
+for item in contents:
+    print(item)
+'''
