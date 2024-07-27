064c08c41b95ab4a40f4b18b132b6e6e911017d9
Sneha N <snehan1807@gmail.com>
2024-07-27 19:21:13 +0530

    Datatypes and Variables

---
+++ Python/chapter-02.py
@@ -0,0 +1,35 @@
+#Question 01
+a = 12
+b = 15
+sum = a +b
+print(sum)
+
+#Question - 02
+num = input("Enter a number to be divided by 2 ")
+num = int(num)
+remainder = num % 2
+print(remainder)
+
+#Question number - 03
+z = "12345"
+print(type(z))
+
+# Question - 04
+x = 34
+y = 80
+print(x>y)
+
+# Question - 05
+inp1 = input("Enter a number x ")
+inp2 = input("Enter a number y ")
+inp1 = int(inp1)
+inp2 = int(inp2)
+avg = (inp1 + inp2)/2
+print(avg)
+
+
+# Question - 06
+num1 = input("Enter a number to find out the square: ")
+num1 = int(num1)
+square = num1 * num1
+print(square)
